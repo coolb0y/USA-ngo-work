a8dde1eed49a87cc63b167efdd71013bf169eb4a
coolb0y <57170752+coolb0y@users.noreply.github.com>
2023-05-01 01:26:50 +0530

    added stream read option

--- server/routes/indexJson.js
+++ server/routes/indexJson.js
@@ -12,24 +12,46 @@ router.get('/', (req, res) => {
   try{
     let jsonpath = req.query.jsonpath;
     const indexname = req.query.indexname;
-    
-        
-    console.log(jsonpath);
-    console.log(indexname);
 
-    // Read the contents of the JSON file
-    //const filePath = jsonpath.replace(/['"]+/g, '');
-    jsonpath =jsonpath.replace(/\\/g, '/');
-    const jsonData = fs.readFileSync(jsonpath, 'utf-8');
-    const parsedData = JSON.parse(jsonData);
+        console.log(jsonpath);
+        console.log(indexname);
 
-    client.index(indexname).addDocuments(parsedData)
-      .then(result =>{ 
-        console.log(result)
-        return res.status(200).send({
-            message: 'Indexing has enqued successfully Please wait for few minutes'
-        })
-    });
+        // Read the contents of the JSON file
+        jsonpath = jsonpath.replace(/\\/g, '/');
+        const highWaterMark = 1024 * 1024 * 50; //50mb
+
+        let jsonData = '';
+        const readStream = fs.createReadStream(jsonpath, { highWaterMark, encoding: 'utf-8' });
+
+        readStream.on('data', (chunk) => {
+          jsonData += chunk;
+        });
+
+        readStream.on('end', () => {
+          const parsedData = JSON.parse(jsonData);
+
+          client.index(indexname).addDocuments(parsedData)
+            .then(result =>{ 
+              console.log(result)
+              return res.status(200).send({
+                  message: 'Indexing has enqued successfully Please wait for few minutes'
+              })
+  })
+  .catch(err => {
+    console.error(err);
+    return res.status(500).send({
+        message: 'Error indexing the data'
+    })
+  });
+});
+
+readStream.on('error', (err) => {
+  console.error(err);
+  return res.status(500).send({
+      message: 'Error reading the JSON file'
+  })
+});
+
   }
   catch(e){
     console.log(e);
